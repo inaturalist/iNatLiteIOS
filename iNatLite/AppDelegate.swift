14199b0b0fed1722f43c4c90b9878eb445127db5
Alex Shepard <alex@inaturalist.org>
2018-03-04 19:49:20 -0800

    smaller font for navbar title

--- iNatLite/AppDelegate.swift
+++ iNatLite/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        if let font = UIFont(name: "Whitney-Medium", size: 24) {
+        if let font = UIFont(name: "Whitney-Medium", size: 20) {
             UINavigationBar.appearance().titleTextAttributes = [
                 NSAttributedStringKey.font: font
             ]
