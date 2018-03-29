2efd1b7504d731710e5c54cdb4e50bf4b57210d2
Alex Shepard <alex@inaturalist.org>
2018-03-29 12:21:38 -0700

    remove commented out code

--- iNatLite/Views/ToastView.swift
+++ iNatLite/Views/ToastView.swift
@@ -12,13 +12,6 @@ class ToastView: UIView {
     @IBOutlet var titleLabel: UILabel?
     @IBOutlet var messageLabel: UILabel?
     @IBOutlet var imageView: UIImageView?
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
     class func instanceFromNib() -> ToastView? {
         let nib = UINib(nibName: "ToastView", bundle: Bundle.main)
