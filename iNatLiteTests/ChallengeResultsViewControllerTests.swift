d420177e83a4ed4690620c6d69902233b1b81bd3
Alex Shepard <alex@inaturalist.org>
2018-04-01 22:27:16 -0700

    add a user image to the test fixtures, and test it

--- iNatLiteTests/ChallengeResultsViewControllerTests.swift
+++ iNatLiteTests/ChallengeResultsViewControllerTests.swift
@@ -13,11 +13,21 @@ class ChallengeResultsViewControllerTests: XCTestCase {
     
     var viewController: ChallengeResultsViewController!
     
+    func imageFromFixture() -> UIImage? {
+        if let path = Bundle(for: ChallengeResultsViewControllerTests.self).path(forResource: "IMG_0766", ofType: "jpg") {
+            return UIImage(contentsOfFile: path)
+        } else {
+            return nil
+        }
+    }
+    
     override func setUp() {
         super.setUp()
         
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         viewController = storyboard.instantiateViewController(withIdentifier: "challengeResults") as! ChallengeResultsViewController
+        viewController.imageFromUser = self.imageFromFixture()
+
         let nav = UINavigationController(rootViewController: viewController)
         UIApplication.shared.keyWindow!.rootViewController = nav
         
@@ -31,6 +41,10 @@ class ChallengeResultsViewControllerTests: XCTestCase {
         super.tearDown()
     }
     
+    func testFixtureImageExists() {
+        XCTAssertNotNil(self.imageFromFixture(), "Image from fixture should not be nil.")
+    }
+    
     func testNotLoaded() {
         viewController.resultsLoaded = false
         viewController.tableView!.reloadData()
@@ -40,7 +54,6 @@ class ChallengeResultsViewControllerTests: XCTestCase {
     }
     
     func testNoTargetNoMatch() {
-        // load an image from a fixture, put it in the vc
         viewController.resultScore = nil
         viewController.targetTaxon = nil
         viewController.resultsLoaded = true
@@ -55,8 +68,10 @@ class ChallengeResultsViewControllerTests: XCTestCase {
         XCTAssertEqual(dividerImage, dividerCell?.dividerImageView?.image, "With no target and no match, the divider cell should display the unknown image")
         
         XCTAssertNil(viewController.imageTaxonCell(), "With No Target and No Match, the taxon image cell should not be displayed")
-        XCTAssertNotNil(viewController.imageCell(), "With no target and no match, the image cell should be displayed")
-        // TODO: test that the imageCell contains the fixture image
+        let imageCell = viewController.imageCell()
+        XCTAssertNotNil(imageCell, "With no target and no match, the image cell should be displayed")
+        // can't seem to test that the image equals the fixture image, perhaps due to resizing/compression?
+        XCTAssertNotNil(imageCell?.userImageView?.image, "With no target, image cell should contain an image from the user.")
         
         
         let actionCell = viewController.actionCell()
